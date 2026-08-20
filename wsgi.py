874029ab5dae67ad4039695f945117fa582617c2
ederalcantara <legacydstudio@gmail.com>
2026-08-19 20:41:34 -0400

    Register quote approval flow and sidebar link

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,12 @@ from app import app
 from calculator import calculator_bp
 from customer_portal import portal_bp
 from online_requests import online_bp
+from quote_flow import quote_flow_bp
 
 app.register_blueprint(calculator_bp)
 app.register_blueprint(portal_bp)
 app.register_blueprint(online_bp)
+app.register_blueprint(quote_flow_bp)
 
 
 @app.after_request
@@ -19,6 +21,8 @@ def inject_sidebar_links(response):
     additions = ''
     if 'href="/online-requests"' not in html:
         additions += '<a class="nav-link" href="/online-requests">Pedidos Online</a>\n    '
+    if 'href="/sales-flow"' not in html:
+        additions += '<a class="nav-link" href="/sales-flow">Fluxo Comercial</a>\n    '
     if 'href="/calculator"' not in html:
         additions += '<a class="nav-link" href="/calculator">Calculadora</a>\n    '
     if marker in html and additions:
