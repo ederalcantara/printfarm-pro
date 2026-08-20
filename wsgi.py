1d5fb05c0edf98833cf5066cd0258af71f69ff67
ederalcantara <legacydstudio@gmail.com>
2026-08-19 20:14:36 -0400

    Register Online Requests admin module

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,28 @@
 from app import app
 from calculator import calculator_bp
 from customer_portal import portal_bp
+from online_requests import online_bp
 
 app.register_blueprint(calculator_bp)
 app.register_blueprint(portal_bp)
+app.register_blueprint(online_bp)
 
 
 @app.after_request
-def inject_calculator_link(response):
-    """Add the calculator link to the existing sidebar without altering app routes."""
+def inject_sidebar_links(response):
     content_type = response.headers.get("Content-Type", "")
     if "text/html" not in content_type:
         return response
 
     html = response.get_data(as_text=True)
     marker = '<a class="nav-link" href="/catalog" target="_blank">Catálogo público ↗</a>'
-    if marker in html and 'href="/calculator"' not in html:
-        calculator_link = '<a class="nav-link" href="/calculator">Calculadora</a>\n    '
-        html = html.replace(marker, calculator_link + marker, 1)
+    additions = ''
+    if 'href="/online-requests"' not in html:
+        additions += '<a class="nav-link" href="/online-requests">Pedidos Online</a>\n    '
+    if 'href="/calculator"' not in html:
+        additions += '<a class="nav-link" href="/calculator">Calculadora</a>\n    '
+    if marker in html and additions:
+        html = html.replace(marker, additions + marker, 1)
         response.set_data(html)
         response.headers["Content-Length"] = str(len(response.get_data()))
     return response
